031ccda64d125a017fae88c4248b9e71a0045dd1
ghassemi <amir.ghassemi@nokia.com>
2026-05-12 13:47:38 -0400

    update keras version with previous code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function
 import os
 import io
+import threading
 import numpy as np
 import tensorflow as tf
 import PIL.Image as Image
@@ -13,58 +14,130 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 ####################################################################################3
-SAVED_MODEL_DIR = os.getcwd() + "/saved_model" # MODEL_PATH
+SAVED_MODEL_DIR = os.getcwd() + "/saved_model"  # MODEL_PATH
 
 TEST_IMAGES_DIR = os.getcwd() + "/image_test"
 
-class_names=np.array(['Abutilon', 'Amarante_a_racine_rouge', 'Chiendent', 'Digitaire_astringente', 'Digitaire_sanguine', 'Echinochloa_pied_de_coq',
-                             'Galinsoga_cilie', 'Laiteron_des_champs', 'Liseron_des_champs', 'Luzerne', 'Morelle_noire_de_l_Est', 'Morelle_poilue',
-                              'Moutarde_des_champs', 'Moutarde_des_oiseaux', 'Ortie_royale', 'Panic_capillaire', 'Panic_d_automne_genicule', 'Petite_herbe_a_poux',
-                               'Phragmite_commun', 'Pissenlit', 'Prele_des_champs', 'Radis_sauvage', 'Renouee_liseron', 'Renouee_persicaire', 'Setaire_geante',
-                                'Setaire_glauque', 'Setaire_verte', 'Souchet_comestible', 'Spargoute_des_champs', 'Stellaire_moyenne', 'Tabouret_des_champs',
-                                 'Vesce_jargeau'])
+class_names = np.array(
+    [
+        "Abutilon",
+        "Amarante_a_racine_rouge",
+        "Chiendent",
+        "Digitaire_astringente",
+        "Digitaire_sanguine",
+        "Echinochloa_pied_de_coq",
+        "Galinsoga_cilie",
+        "Laiteron_des_champs",
+        "Liseron_des_champs",
+        "Luzerne",
+        "Morelle_noire_de_l_Est",
+        "Morelle_poilue",
+        "Moutarde_des_champs",
+        "Moutarde_des_oiseaux",
+        "Ortie_royale",
+        "Panic_capillaire",
+        "Panic_d_automne_genicule",
+        "Petite_herbe_a_poux",
+        "Phragmite_commun",
+        "Pissenlit",
+        "Prele_des_champs",
+        "Radis_sauvage",
+        "Renouee_liseron",
+        "Renouee_persicaire",
+        "Setaire_geante",
+        "Setaire_glauque",
+        "Setaire_verte",
+        "Souchet_comestible",
+        "Spargoute_des_champs",
+        "Stellaire_moyenne",
+        "Tabouret_des_champs",
+        "Vesce_jargeau",
+    ]
+)
 ####################################################################################
 
-# # Load your trained model
-model = tf.keras.models.load_model(SAVED_MODEL_DIR)
+MODEL_ENDPOINT = "serving_default"
 
-@app.route('/', methods=['GET'])
+
+def load_inference_graph(saved_model_dir):
+    graph = tf.Graph()
+    with graph.as_default():
+        session = tf.compat.v1.Session(graph=graph)
+        meta_graph = tf.compat.v1.saved_model.loader.load(
+            session,
+            [tf.saved_model.SERVING],
+            saved_model_dir,
+        )
+
+    signature = meta_graph.signature_def[MODEL_ENDPOINT]
+    input_tensor_name = signature.inputs["keras_layer_input"].name
+    output_tensor_name = next(iter(signature.outputs.values())).name
+
+    return (
+        session,
+        graph.get_tensor_by_name(input_tensor_name),
+        graph.get_tensor_by_name(output_tensor_name),
+    )
+
+
+session, input_tensor, output_tensor = load_inference_graph(SAVED_MODEL_DIR)
+session_lock = threading.Lock()
+
+
+def predict_scores(batch):
+    with session_lock:
+        return session.run(
+            output_tensor,
+            feed_dict={input_tensor: np.asarray(batch, dtype=np.float32)},
+        )
+
+
+@app.route("/", methods=["GET"])
 def index():
     # Main page
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/predict", methods=["GET", "POST"])
 def upload():
-    print('request.files: ',request.files)
+    print("request.files: ", request.files)
     if request.method == "POST":
-        file = request.files.get('file')
+        file = request.files.get("file")
         if file is None or file.filename == "":
             return jsonify({"error": "no file"})
-        
+
         try:
             image_bytes = file.read()
             # print('image_bytes: ', type(image_bytes))
             pillow_img = Image.open(io.BytesIO(image_bytes))
             # print('pillow_img: ', type(pillow_img))
 
-            resize_image= pillow_img.resize((224,224))
-            array_image = np.array(resize_image)/255
+            resize_image = pillow_img.resize((224, 224))
+            array_image = np.array(resize_image) / 255
 
-            predicted = model.predict(np.array([array_image]))
+            predicted = predict_scores(
+                np.array([array_image], dtype=np.float32)
+            )
             predicted_max_index = np.argmax(predicted, axis=-1)
             predicted_label_batch = class_names[predicted_max_index]
 
-            conf=np.max(predicted, axis=-1)*100
+            conf = np.max(predicted, axis=-1) * 100
 
             confidence = round(conf[0], 2)
-            result= str(predicted_label_batch[0]) + '    ' + '(Confidence:    ' + str(confidence) + ' % ' + ')'
-            return result 
+            result = (
+                str(predicted_label_batch[0])
+                + "    "
+                + "(Confidence:    "
+                + str(confidence)
+                + " % "
+                + ")"
+            )
+            return result
         except Exception as e:
             return jsonify({"error": str(e)})
-    return None # "OK"
+    return None  # "OK"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
     # app.run(debug=True) ## Default
-
